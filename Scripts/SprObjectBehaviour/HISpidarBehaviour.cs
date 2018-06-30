827de347d0df440cd1da7ab9981397ba4cf1e7e2
mitake-h <mitake@haselab.net>
2018-06-30 16:05:30 +0900

    Add Raw Spidar API

--- Scripts/SprObjectBehaviour/HISpidarBehaviour.cs
+++ Scripts/SprObjectBehaviour/HISpidarBehaviour.cs
@@ -5,21 +5,22 @@ using SprCs;
 using SprUnity;
 
 [DefaultExecutionOrder(10)]
-public class HISpidarGBehaviour : SprBehaviour {
+public class HISpidarBehaviour : SprBehaviour {
     static HISdkIf hiSdk = null;
 
     // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
     // メンバ変数
 
-    public HISpidarGDescStruct desc;
+    public HISpidarDescStruct desc;
 
-    public GameObject pointer = null;
+    public int nMotors = 1;
+
     public TextMesh lengthText = null;
 
     // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
     // このBehaviourに対応するSpringheadオブジェクト
 
-    public HISpidarGIf hiSpidar { get { return sprObject as HISpidarGIf; } }
+    public HISpidarIf hiSpidar { get { return sprObject as HISpidarIf; } }
 
     // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
     // SprBehaviourの派生クラスで実装するメソッド
@@ -27,7 +28,7 @@ public class HISpidarGBehaviour : SprBehaviour {
     // -- DescStructオブジェクトを再構築する
     public override void ResetDescStruct() {
         // <!!> とりあえず作るけど実際には使わない。作らなくてもいいのなら作らないほうがいい
-        desc = (HISpidarGDescStruct)(new HISpidarGDesc("SpidarG6X3R"));
+        // desc = (HISpidarGDescStruct)(new HISpidarGDesc("SpidarG6X3R"));
     }
 
     // -- DescStructオブジェクトを取得する
@@ -37,12 +38,12 @@ public class HISpidarGBehaviour : SprBehaviour {
 
     // -- DescオブジェクトをNewして返す
     public override CsObject CreateDesc() {
-        return new HISpidarGDesc("SpidarG6X3R");
+        return new HISpidarDesc();
     }
 
     // -- DescStructをDescに適用する
     public override void ApplyDesc(CsObject from, CsObject to) {
-        (from as HISpidarGDescStruct).ApplyTo(to as HISpidarGDesc);
+        // (from as HISpidarGDescStruct).ApplyTo(to as HISpidarGDesc);
     }
 
     // -- Sprオブジェクトの構築を行う
@@ -55,9 +56,13 @@ public class HISpidarGBehaviour : SprBehaviour {
             hiSdk.AddRealDevice(DRCyUsb20Sh4If.GetIfInfoStatic(), cyDesc);
         }
 
-        var spg = hiSdk.CreateHumanInterface(HISpidarGIf.GetIfInfoStatic());
+        var spg = hiSdk.CreateHumanInterface(HISpidarIf.GetIfInfoStatic());
 
-        var d = new HISpidarGDesc("SpidarG6X3R"); // do not use "desc"
+        var d = new HISpidarDesc(); // do not use "desc"
+        for (int i = 0; i < nMotors; i++) {
+            var dM = new HISpidarMotorDesc();
+            d.motors.push_back(dM);
+        }
         spg.Init(d);
         spg.Calibration();
 
@@ -75,14 +80,9 @@ public class HISpidarGBehaviour : SprBehaviour {
                 string text = "";
                 for (int i = 0; i < hiSpidar.NMotor(); i++) {
                     text += hiSpidar.GetMotor((uint)i).GetLength().ToString() + "\r\n";
+                    hiSpidar.GetMotor((uint)i).SetForce(1.0f);
                 }
                 lengthText.text = text;
-            }
-
-            if (pointer != null) {
-                Posed pose = hiSpidar.GetPose();
-                pointer.transform.position = pose.Pos().ToVector3();
-                pointer.transform.rotation = pose.Ori().ToQuaternion();
             }
         }
     }
