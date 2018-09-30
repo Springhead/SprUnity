3d076cdb3c09d063549b0d678dcf75617dd08e99
mitake-h <mitake@haselab.net>
2018-10-01 02:09:46 +0900

    まぶた（まばたきとにっこり）

--- Scripts/Creature/PhysicalBodyModel/Face.cs
+++ Scripts/Creature/PhysicalBodyModel/Face.cs
@@ -25,6 +25,11 @@ namespace InteraWare {
 
         // ----- ----- ----- ----- -----
 
+        public float blinkClose = 0.0f;
+        public float smileClose = 0.0f;
+
+        // ----- ----- ----- ----- -----
+
         private Vector3 leftLowerEyeBasePos = new Vector3();
         private Vector3 leftUpperEyeBasePos = new Vector3();
         private Vector3 rightLowerEyeBasePos = new Vector3();
@@ -57,33 +62,47 @@ namespace InteraWare {
 
                 // ----- ----- ----- ----- -----
 
+                var lowerBlinkCloseOffset = new Vector3(0, 0.01f, 0);
+                var lowerSmileCloseOffset = new Vector3(0, 0.025f, 0.00375f);
+
+                var upperBlinkCloseOffset = new Vector3(0, -0.03f, 0);
+                var upperSmileCloseOffset = new Vector3(0, -0.015f, 0.00375f);
+
+                float s = 1.0f;
+                if (blinkClose + smileClose > 1) {
+                    s = (blinkClose + smileClose);
+                }
+
+                Vector3 lowerCloseOffset = (blinkClose * lowerBlinkCloseOffset + smileClose * lowerSmileCloseOffset) * (1 / s);
+                Vector3 upperCloseOffset = (blinkClose * upperBlinkCloseOffset + smileClose * upperSmileCloseOffset) * (1 / s);
+
                 Vector3 leftLowerEyeOffset = new Vector3(
                     Mathf.Clamp(-angleL.x * lowerCoeff.x, lowerLimitMin.x, lowerLimitMax.x),
                     Mathf.Clamp(+angleL.y * lowerCoeff.y, lowerLimitMin.y, lowerLimitMax.y),
                     Mathf.Clamp(+angleL.y * lowerCoeff.y * lowerCoeff.z, lowerLimitMin.z, lowerLimitMax.z)
                     );
-                leftLowerEye.transform.localPosition = leftLowerEyeBasePos + leftLowerEyeOffset;
+                leftLowerEye.transform.localPosition = leftLowerEyeBasePos + leftLowerEyeOffset + lowerCloseOffset;
 
                 Vector3 leftUpperEyeOffset = new Vector3(
                     Mathf.Clamp(-angleL.x * upperCoeff.x, upperLimitMin.x, upperLimitMax.x),
                     Mathf.Clamp(+angleL.y * upperCoeff.y, upperLimitMin.y, upperLimitMax.y),
                     Mathf.Clamp(+angleL.y * upperCoeff.y * upperCoeff.z, upperLimitMin.z, upperLimitMax.z)
                     );
-                leftUpperEye.transform.localPosition = leftUpperEyeBasePos + leftUpperEyeOffset;
+                leftUpperEye.transform.localPosition = leftUpperEyeBasePos + leftUpperEyeOffset + upperCloseOffset;
 
                 Vector3 rightLowerEyeOffset = new Vector3(
                     Mathf.Clamp(-angleR.x * lowerCoeff.x, lowerLimitMin.x, lowerLimitMax.x),
                     Mathf.Clamp(+angleR.y * lowerCoeff.y, lowerLimitMin.y, lowerLimitMax.y),
                     Mathf.Clamp(+angleR.y * lowerCoeff.y * lowerCoeff.z, lowerLimitMin.z, lowerLimitMax.z)
                     );
-                rightLowerEye.transform.localPosition = rightLowerEyeBasePos + rightLowerEyeOffset;
+                rightLowerEye.transform.localPosition = rightLowerEyeBasePos + rightLowerEyeOffset + lowerCloseOffset;
 
                 Vector3 rightUpperEyeOffset = new Vector3(
                     Mathf.Clamp(-angleR.x * upperCoeff.x, upperLimitMin.x, upperLimitMax.x),
                     Mathf.Clamp(+angleR.y * upperCoeff.y, upperLimitMin.y, upperLimitMax.y),
                     Mathf.Clamp(+angleR.y * upperCoeff.y * upperCoeff.z, upperLimitMin.z, upperLimitMax.z)
                     );
-                rightUpperEye.transform.localPosition = rightUpperEyeBasePos + rightUpperEyeOffset;
+                rightUpperEye.transform.localPosition = rightUpperEyeBasePos + rightUpperEyeOffset + upperCloseOffset;
             }
         }
     }
