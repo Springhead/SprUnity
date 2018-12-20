140c86102eaf2cd53e92ddef6ff8da98c3ea2c56
sugiken0 <sugimori.k.ac@m.titech.ac.jp>
2018-12-20 22:44:47 +0900

    IKが一つ飛ばしでついていても動くようにしたがうまく動作していない？

--- Scripts/SprObjectBehaviour/PHIKActuatorBehaviour.cs
+++ Scripts/SprObjectBehaviour/PHIKActuatorBehaviour.cs
@@ -50,21 +50,30 @@ public abstract class PHIKActuatorBehaviour : SprSceneObjBehaviour {
     public override void Link() {
         // まず、このIKActuatorに付随する関節を探す。同じGameObjectに付いているはず
         PHJointBehaviour jo = gameObject.GetComponent<PHJointBehaviour>();
-
+        
         if (jo != null && jo.sprObject != null && sprObject != null) {
             // 次に、関節の親関節を探す。関節のソケット剛体を探し、それを基準に探す
             // （親関節　＝　この関節のソケット剛体をプラグ剛体として持つ関節）
-            PHJointBehaviour joParent = null;
-            var jos = jo.socket.GetComponentsInChildren<PHJointBehaviour>();
-            foreach (var j in jos) {
-                if (j.plug == jo.socket) { joParent = j; break; }
-            }
+            // 親関節がないかどんどん上に行って探す
+            while (true) {
+                PHJointBehaviour joParent = null;
+                var jos = jo.socket.GetComponentsInChildren<PHJointBehaviour>(); //InChildrenで探す必要はないはず
+                foreach (var j in jos) {
+                    if (j.plug == jo.socket) { joParent = j; break; }
+                }
 
-            if (joParent != null && jo != joParent) {
-                // 親関節に付随するIKActuatorを親Actuatorとして登録する
-                PHIKActuatorBehaviour act = joParent.GetComponent<PHIKActuatorBehaviour>();
-                if (act != null && act.sprObject != null && sprObject != act.sprObject) {
-                    act.sprObject.AddChildObject(sprObject);
+                if (joParent != null && jo != joParent) {
+                    // 親関節に付随するIKActuatorを親Actuatorとして登録する
+                    PHIKActuatorBehaviour act = joParent.GetComponent<PHIKActuatorBehaviour>();
+                    if (act != null && act.sprObject != null && sprObject != act.sprObject) {
+                        act.sprObject.AddChildObject(sprObject);
+                        break;
+                    }
+                    if(act == null) {
+                        jo = joParent;
+                    }
+                } else {
+                    break;
                 }
             }
         }
