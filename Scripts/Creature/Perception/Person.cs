6216e0e6d382ace4bc8ec016ab91c5f6feb1ce7c
mitake-h <mitake@haselab.net>
2019-03-23 18:43:55 +0900

    reverted some changes (but nessesary for AGCSVG... will be changed again later)

--- Scripts/Creature/Perception/Person.cs
+++ Scripts/Creature/Perception/Person.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System;
 using UnityEngine;
 
@@ -9,12 +8,15 @@ public class Person : MonoBehaviour {
     public static List<Person> persons = new List<Person>();
 
     // ----- ----- ----- ----- -----
-
-    public string id = "";
-
-    // ----- ----- ----- ----- -----
     // Basic Setting
     public bool human = true;
+
+    public bool ignoredByAttention = false; // このフラグが有効な場合はAttentionの対象としては無視される
+
+    public bool autoEliminate = false; // このPersonは自動的に消滅する
+    public float autoEliminateTime = -1; // [秒]
+
+	public DateTime createdTime;
 
     // ----- ----- ----- ----- -----
     // Common Sensor Result
@@ -24,64 +26,103 @@ public class Person : MonoBehaviour {
     public GameObject leftHand = null;
     public GameObject rightHand = null;
 
+    // -- Facial Expression
+    // <TBD>
+
+    // -- Gaze
+    public Vector3 gaze = new Vector3(0, 0, -1); // in 頭ローカル座標系
+
+    // -- Voice
+    public float volume = 0.0f;
+    public float pitch = 0.0f;
+
     // ----- ----- ----- ----- -----
-    // Attributes
-	public class Attribute { public virtual void OnDrawGizmos(Person person) { } }
-    public Dictionary<Type, Attribute> attributes = new Dictionary<Type, Attribute>();
-    public Type GetAttr<Type>() where Type : Attribute, new() {
-        if (attributes.ContainsKey(typeof(Type))) {
-            return (attributes[typeof(Type)] as Type);
+    // Individual Sensor Information
+    public class SensorInfo { public virtual void OnDrawGizmos(Person person) { } }
+    public Dictionary<Type, SensorInfo> sensorInfo = new Dictionary<Type, SensorInfo>();
+    public Type GetSensorInfo<Type> () where Type : SensorInfo {
+        return (sensorInfo.ContainsKey(typeof(Type))) ? (sensorInfo[typeof(Type)] as Type) : null;
+    }
+    public Type AddSensorInfo<Type>() where Type : SensorInfo, new() {
+        if (sensorInfo.ContainsKey(typeof(Type))) {
+            return (sensorInfo[typeof(Type)] as Type);
         } else {
             Type newObj = new Type();
-            attributes[typeof(Type)] = newObj;
+            sensorInfo[typeof(Type)] = newObj;
+            return newObj;
+        }
+    }
+
+    // ----- ----- ----- ----- -----
+    // Perception (processed information from common sensor result)
+    public class Perception { public virtual void OnDrawGizmos(Person person) { } }
+    public Dictionary<Type, Perception> perception = new Dictionary<Type, Perception>();
+    public Type GetPerception<Type>() where Type : Perception {
+        return (perception.ContainsKey(typeof(Type))) ? (perception[typeof(Type)] as Type) : null;
+    }
+    public Type AddPerception<Type>() where Type : Perception, new() {
+        if (perception.ContainsKey(typeof(Type))) {
+            return (perception[typeof(Type)] as Type);
+        } else {
+            Type newObj = new Type();
+            perception[typeof(Type)] = newObj;
+            return newObj;
+        }
+    }
+
+    // ----- ----- ----- ----- -----
+    // State Value for Character AI
+	public class StateValue { public virtual void OnDrawGizmos(Person person) { } }
+    public Dictionary<Type, StateValue> stateValue = new Dictionary<Type, StateValue>();
+    public Type GetStateValue<Type>() where Type : StateValue {
+        return (stateValue.ContainsKey(typeof(Type))) ? (stateValue[typeof(Type)] as Type) : null;
+    }
+    public Type AddStateValue<Type>() where Type : StateValue, new() {
+        if (stateValue.ContainsKey(typeof(Type))) {
+            return (stateValue[typeof(Type)] as Type);
+        } else {
+            Type newObj = new Type();
+            stateValue[typeof(Type)] = newObj;
             return newObj;
         }
     }
 
     // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 
-    private TextMesh debugTextMesh = null;
     void Start () {
-        id = Guid.NewGuid().ToString("N").Substring(0, 10);
         persons.Add(this);
+		createdTime = DateTime.Now;
+
         if (head == null) { head = gameObject; }
-
-        // <!!>
-        if (human) {
-            GameObject prefab = (GameObject)Resources.Load("Prefabs/PersonDebugInfo");
-            var debugObj = Instantiate(prefab, new Vector3(), Quaternion.identity);
-            debugObj.transform.parent = this.transform;
-            debugObj.transform.localPosition = new Vector3();
-            debugTextMesh = debugObj.transform.Find("Text").gameObject.GetComponent<TextMesh>();
-        }
-    }
-
-    void Update() {
-        // <!!>
-        if (human) {
-            var netAttr = GetAttr<Network.Attribute>();
-            string debugText = "";
-            debugText += "id : " + id + "\r\n";
-            debugText += "helmet : " + netAttr.helmet + "\r\n";
-            debugText += "vip : " + netAttr.vip + "\r\n";
-            debugText += "appoint : " + netAttr.appointmentStatus + "\r\n";
-            debugText += "name : " + netAttr.personName + "\r\n";
-            if (debugTextMesh != null) {
-                debugTextMesh.text = debugText;
-            }
-        }
-    }
-
-    void FixedUpdate () {
+	}
+	
+	void FixedUpdate () {
 		// 位置追従
 		if (head != null && head != gameObject) {
 			gameObject.transform.position = head.transform.position;
 			gameObject.transform.rotation = head.transform.rotation;
 		}
+
+		if (head == null && !autoEliminate) {
+			autoEliminate = true;
+			autoEliminateTime = 0.5f;
+		}
+
+        // 自動消滅処理
+        if (autoEliminate) {
+            if (autoEliminateTime > 0) {
+                autoEliminateTime -= Time.fixedDeltaTime;
+            } else {
+				Destroy (this.gameObject);
+            }
+        }
     }
 
     void OnDrawGizmos() {
         if (head != null) { 
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(head.transform.position, head.transform.position + gaze);
+
             Gizmos.color = Color.gray;
             Gizmos.DrawWireSphere(head.transform.position, 0.1f);
             var footPos = head.transform.position; footPos.y = 0;
@@ -96,13 +137,18 @@ public class Person : MonoBehaviour {
 			Gizmos.DrawWireSphere(rightHand.transform.position, 0.1f);
 		}
 
-        foreach(var kv in attributes) {
+        foreach(var kv in sensorInfo) {
             kv.Value.OnDrawGizmos(this);
         }
+        foreach (var kv in perception) {
+            kv.Value.OnDrawGizmos(this);
+        }
+		foreach (var kv in stateValue) {
+			kv.Value.OnDrawGizmos(this);
+		}
     }
 
     void OnDestroy() {
-        Debug.Log("Remove Person");
         persons.Remove(this);
     }
 
