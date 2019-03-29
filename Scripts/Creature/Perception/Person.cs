1b83cfd3ce924664db4709f16b7c4d6e6004d3e2
mitake <mitake@pi.titech.ac.jp>
2019-03-29 22:14:24 +0900

    something

--- Scripts/Creature/Perception/Person.cs
+++ Scripts/Creature/Perception/Person.cs
@@ -26,7 +26,7 @@ public class Person : MonoBehaviour {
 
     // ----- ----- ----- ----- -----
     // Attributes
-	public class Attribute { public virtual void OnDrawGizmos(Person person) { } }
+    public class Attribute { public virtual void OnDrawGizmos(Person person) { } }
     public Dictionary<Type, Attribute> attributes = new Dictionary<Type, Attribute>();
     public Type GetAttr<Type>() where Type : Attribute, new() {
         if (attributes.ContainsKey(typeof(Type))) {
@@ -38,10 +38,15 @@ public class Person : MonoBehaviour {
         }
     }
 
+    // ----- ----- ----- ----- -----
+    // Visualize
+    public GameObject visualizeObject = null;
+    public bool visualize = false;
+
     // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 
     private TextMesh debugTextMesh = null;
-    void Start () {
+    void Start() {
         id = Guid.NewGuid().ToString("N").Substring(0, 10);
         persons.Add(this);
         if (head == null) { head = gameObject; }
@@ -49,10 +54,11 @@ public class Person : MonoBehaviour {
         // <!!>
         if (human) {
             GameObject prefab = (GameObject)Resources.Load("Prefabs/PersonDebugInfo");
-            var debugObj = Instantiate(prefab, new Vector3(), Quaternion.identity);
-            debugObj.transform.parent = this.transform;
-            debugObj.transform.localPosition = new Vector3();
-            debugTextMesh = debugObj.transform.Find("Text").gameObject.GetComponent<TextMesh>();
+            visualizeObject = Instantiate(prefab, new Vector3(), Quaternion.identity);
+            visualizeObject.transform.parent = FindObjectOfType<Camera>().transform;
+            visualizeObject.transform.localPosition = new Vector3();
+            debugTextMesh = visualizeObject.transform.Find("Text").gameObject.GetComponent<TextMesh>();
+            SetVisualize(visualize);
         }
     }
 
@@ -72,38 +78,54 @@ public class Person : MonoBehaviour {
         }
     }
 
-    void FixedUpdate () {
-		// 位置追従
-		if (head != null && head != gameObject) {
-			gameObject.transform.position = head.transform.position;
-			gameObject.transform.rotation = head.transform.rotation;
-		}
+    void FixedUpdate() {
+        // 位置追従
+        if (head != null && head != gameObject) {
+            gameObject.transform.position = head.transform.position;
+            gameObject.transform.rotation = head.transform.rotation;
+        }
+
+        // ビジュアライザの位置追従
+        if (visualizeObject != null) {
+            visualizeObject.transform.localPosition = new Vector3(transform.position.x * 0.2f, transform.position.z * 0.2f - 0.3f, 2.0f);
+        }
     }
 
     void OnDrawGizmos() {
-        if (head != null) { 
+        if (head != null) {
             Gizmos.color = Color.gray;
             Gizmos.DrawWireSphere(head.transform.position, 0.1f);
             var footPos = head.transform.position; footPos.y = 0;
             Gizmos.DrawLine(head.transform.position, footPos);
         }
-		if (leftHand != null) {
-			Gizmos.color = Color.gray;
-			Gizmos.DrawWireSphere(leftHand.transform.position, 0.1f);
-		}
-		if (rightHand != null) {
-			Gizmos.color = Color.gray;
-			Gizmos.DrawWireSphere(rightHand.transform.position, 0.1f);
-		}
+        if (leftHand != null) {
+            Gizmos.color = Color.gray;
+            Gizmos.DrawWireSphere(leftHand.transform.position, 0.1f);
+        }
+        if (rightHand != null) {
+            Gizmos.color = Color.gray;
+            Gizmos.DrawWireSphere(rightHand.transform.position, 0.1f);
+        }
 
-        foreach(var kv in attributes) {
+        foreach (var kv in attributes) {
             kv.Value.OnDrawGizmos(this);
         }
     }
 
     void OnDestroy() {
-        Debug.Log("Remove Person");
+        if (visualizeObject != null) {
+            Destroy(visualizeObject);
+        }
         persons.Remove(this);
+    }
+
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+    public void SetVisualize(bool visualize) {
+        if (visualizeObject != null) {
+            this.visualize = visualize;
+            visualizeObject.SetActive(this.visualize);
+        }
     }
 
 }
