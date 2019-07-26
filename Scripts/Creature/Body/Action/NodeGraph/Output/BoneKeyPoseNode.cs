b6c3bdccf8a18646dac33a4bacd6cc3900b0ba1e
Sugi <sugimori.k.ac@m.titech.ac.jp>
2019-07-26 09:58:27 +0900

    RelativePosRotScaleNodeを選択した時に編集ポリゴンを表示するようにした

--- Scripts/Creature/Body/Action/NodeGraph/Output/BoneKeyPoseNode.cs
+++ Scripts/Creature/Body/Action/NodeGraph/Output/BoneKeyPoseNode.cs
@@ -16,17 +16,6 @@ namespace SprUnity {
         [Input] public bool usePosition;
         [Input] public bool useRotation;
 
-        private Material editableMat, visibleMat;
-        private Mesh leftHand;
-        private Mesh rightHand;
-        private Mesh head;
-        private Mesh leftFoot;
-        private Mesh rightFoot;
-
-        private float handleSize = 0.05f;
-        private float selectedHandleSize = 0.15f;
-        private StaticBoneKeyPose selectedboneKeyPose; // マウスが上にあるKeyPoseだけハンドルを大きくする
-
         // Use this for initialization
         protected override void Init() {
             base.Init();
@@ -63,50 +52,6 @@ namespace SprUnity {
             return keyPose;
         }
 
-        protected new void OnEnable() {
-            base.OnEnable();
-
-            var modelpath = "Assets/Libraries/SprUnity/Editor/Creature/Models/";
-
-            editableMat = AssetDatabase.LoadAssetAtPath(modelpath + "editable.mat", typeof(Material)) as Material;
-            visibleMat = AssetDatabase.LoadAssetAtPath(modelpath + "visible.mat", typeof(Material)) as Material;
-            if (editableMat == null) {
-                Debug.Log("mat null");
-            }
-            if (visibleMat == null) {
-                Debug.Log("mat null");
-            }
-
-            leftHand = AssetDatabase.LoadAssetAtPath(
-                modelpath + "LeftHand.fbx", typeof(Mesh)) as Mesh;
-            if (leftHand == null) {
-                Debug.Log("fbx null");
-            }
-
-            rightHand = AssetDatabase.LoadAssetAtPath(
-                modelpath + "RightHand.fbx", typeof(Mesh)) as Mesh;
-            if (rightHand == null) {
-                Debug.Log("fbx null");
-            }
-
-            head = AssetDatabase.LoadAssetAtPath(
-                modelpath + "Head.fbx", typeof(Mesh)) as Mesh;
-            if (head == null) {
-                Debug.Log("fbx null");
-            }
-
-            leftFoot = AssetDatabase.LoadAssetAtPath(
-                modelpath + "LeftFoot.fbx", typeof(Mesh)) as Mesh;
-            if (leftFoot == null) {
-                Debug.Log("fbx null");
-            }
-
-            rightFoot = AssetDatabase.LoadAssetAtPath(
-                modelpath + "RightFoot.fbx", typeof(Mesh)) as Mesh;
-            if (rightFoot == null) {
-                Debug.Log("fbx null");
-            }
-        }
         public override void OnSceneGUI(Body body = null) {
             BoneKeyPose temp = GetBoneKeyPose();
 #if UNITY_EDITOR
@@ -124,38 +69,6 @@ namespace SprUnity {
 #endif
         }
 
-        void DrawHumanBone(BoneKeyPose boneKeyPose) {
-            if (boneKeyPose.usePosition || boneKeyPose.useRotation) {
-                // 調整用の手などを表示
-                editableMat.SetPass(0); // 1だと影しか見えない？ 
-                if (boneKeyPose.boneId == HumanBodyBones.LeftHand) {
-                    Graphics.DrawMeshNow(leftHand, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                } else if (boneKeyPose.boneId == HumanBodyBones.RightHand) {
-                    Graphics.DrawMeshNow(rightHand, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                } else if (boneKeyPose.boneId == HumanBodyBones.Head) {
-                    Graphics.DrawMeshNow(head, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                } else if (boneKeyPose.boneId == HumanBodyBones.LeftFoot) {
-                    Graphics.DrawMeshNow(leftFoot, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                } else if (boneKeyPose.boneId == HumanBodyBones.RightFoot) {
-                    Graphics.DrawMeshNow(rightFoot, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                }
-            }
-            if (boneKeyPose.usePosition || boneKeyPose.useRotation) {
-                // 調整用の手などを表示
-                visibleMat.SetPass(0); // 1だと影しか見えない？ 
-                if (boneKeyPose.boneId == HumanBodyBones.LeftHand) {
-                    Graphics.DrawMeshNow(leftHand, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                } else if (boneKeyPose.boneId == HumanBodyBones.RightHand) {
-                    Graphics.DrawMeshNow(rightHand, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                } else if (boneKeyPose.boneId == HumanBodyBones.Head) {
-                    Graphics.DrawMeshNow(head, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                } else if (boneKeyPose.boneId == HumanBodyBones.LeftFoot) {
-                    Graphics.DrawMeshNow(leftFoot, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                } else if (boneKeyPose.boneId == HumanBodyBones.RightFoot) {
-                    Graphics.DrawMeshNow(rightFoot, boneKeyPose.position, boneKeyPose.rotation.normalized, 0);
-                }
-            }
-        }
     }
 
 }
