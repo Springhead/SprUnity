ba8a686d6a81c6c4dba8d88931d17027221e22c0
sugiken0 <sugimori.k.ac@m.titech.ac.jp>
2019-07-31 19:13:29 +0900

    MentalSceneをVRMAgentからSprUnityに移動

---
+++ Scripts/Creature/MentalScene/MentalScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SprUnity;
+
+namespace SprUnity {
+    public class MentalScene : MonoBehaviour {
+        public MentalGroup[] mentalGroups {
+            get {
+                return FindObjectsOfType<MentalGroup>();
+            }
+        }
+        public MentalObject[] mentalObjects {
+            get {
+                return FindObjectsOfType<MentalObject>();
+            }
+        }
+    }
+}
