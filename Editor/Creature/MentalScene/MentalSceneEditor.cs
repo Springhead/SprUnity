dbf8bd894cf3df4a1bd377169cd79e1bcea1142d
Sugi <sugimori.k.ac@m.titech.ac.jp>
2019-08-01 22:50:02 +0900

    KeyPoseWindowのパスを修正 MentalSceneEditorからOnSceneGUIが呼べている

--- Editor/Creature/MentalScene/MentalSceneEditor.cs
+++ Editor/Creature/MentalScene/MentalSceneEditor.cs
@@ -6,5 +6,8 @@ using UnityEditor;
 namespace SprUnity {
     [CustomEditor(typeof(MentalScene))]
     public class MentalSceneEditor : Editor{
+        private void OnSceneGUI() {
+            Handles.PositionHandle(new Vector3(1,0,0), Quaternion.identity);
+        }
     }
 }
