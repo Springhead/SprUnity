c75d05c05f09876c74ad7ee592b8a012b742cbb5
sprphys <order@gs.haselab.net>
2024-02-19 17:43:40 +0900

    コロコロがUnityで動くためのHIKorokoroBehaviorを追加

---
+++ Scripts/Framework/FWHapticAppBehaviourKorokoro.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SprCs;
+using SprUnity;
+
+[DefaultExecutionOrder(0)]
+public class FWHapticAppBehaviourKorokoro : MonoBehaviour
+{
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // メンバ変数
+
+    public static FWHapticApp app = null;
+
+    public PHSceneBehaviour phSceneBehaviour;
+    public HIKorokoroBehaviour hiKorokoroBehaviour;
+
+    public float spring;
+    public float damper;
+
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // Privateメンバ
+
+    private PHHapticPointerIf pointer;
+    private GameObject pointerObj;
+
+    private Transform pointerTransform;
+    private float beforespring;
+    private float beforedamper;
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MonoBehaviourのメソッド
+
+    void Start()
+    {
+        app = new FWHapticApp();
+
+        app.CreateSdk();
+        app.GetSdk().CreateScene();
+        app.SetPHScene(phSceneBehaviour.phScene);
+
+        app.CreateTimers();
+
+        //
+
+        phSceneBehaviour.enableStep = false;
+
+        //
+
+        phSceneBehaviour.phScene.GetHapticEngine().Enable(true);
+        phSceneBehaviour.phScene.GetHapticEngine().SetHapticStepMode(PHHapticEngineDesc.HapticStepMode.LOCAL_DYNAMICS);
+
+        //
+
+        pointer = phSceneBehaviour.phScene.CreateHapticPointer();
+
+        var shapeDesc = new CDSphereDesc();
+        shapeDesc.radius = 0.05f;
+        shapeDesc.material.mu = 0;
+        shapeDesc.material.mu0 = 0;
+        var shape = phSceneBehaviour.phScene.GetSdk().CreateShape(CDSphereIf.GetIfInfoStatic(), shapeDesc);
+        pointer.AddShape(shape);
+        pointer.SetHapticRenderMode(PHHapticPointerDesc.HapticRenderMode.CONSTRAINT);
+        //pointer.SetLocalRange(1.0f);
+        //pointer.SetPosScale(1);
+        pointer.EnableForce(true);
+
+        var fwPointer = app.GetSdk().GetScene(0).CreateHapticPointer();
+        fwPointer.SetHumanInterface(hiKorokoroBehaviour.hiKorokoro);
+        fwPointer.SetPHHapticPointer(pointer);
+
+        pointer.SetReflexSpring(spring);
+        pointer.SetReflexDamper(damper);
+        pointer.SetFrictionSpring(0f);
+
+        // 
+
+        pointerObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        pointerObj.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        // 
+
+        app.GetTimer(0).SetInterval(10);
+        app.GetTimer(1).SetMode(UTTimerIf.Mode.MULTIMEDIA);
+        app.GetTimer(1).SetResolution(1);
+        app.GetTimer(1).SetInterval(1);
+        app.StartTimers();
+
+        //
+
+        pointerTransform = GameObject.Find("Pointer").transform;
+        
+        //
+    }
+
+    void FixedUpdate()
+    {
+        app.GetSdk().GetScene(0).GetPHScene().GetHapticEngine().StepPhysicsSimulation();
+        //pointerObj.transform.position = pointer.GetPose().Pos().ToVector3();
+        pointerObj.transform.position = pointerTransform.position;
+        if (spring !=beforespring)
+        {
+            pointer.SetReflexSpring(spring);
+        }
+        beforespring = spring;
+        if (damper != beforedamper)
+        {
+            pointer.SetReflexDamper(damper);
+        }
+        beforedamper = damper;
+
+
+    }
+}
---
+++ Scripts/HumanInterface/HIKorokoroBehaviour.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SprCs;
+using SprUnity;
+using UnityEditor;
+
+[DefaultExecutionOrder(10)]
+public class HIKorokoroBehaviour : SprBehaviour
+{
+    static HISdkIf hiSdk = null;
+
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // メンバ変数
+
+    public HIKorokoroDescStruct desc;
+
+    public GameObject pointer = null;
+    public TextMesh lengthText = null;
+
+    private Transform pointerTransform;
+    private Transform controllerTransform;
+
+    public int nMotors = 3;
+
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // このBehaviourに対応するSpringheadオブジェクト
+
+    public HIKorokoroIf hiKorokoro { get { return sprObject as HIKorokoroIf; } }
+
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // SprBehaviourの派生クラスで実装するメソッド
+
+    // -- DescStructオブジェクトを再構築する
+    public override void ResetDescStruct()
+    {
+        // <!!> とりあえず作るけど実際には使わない。作らなくてもいいのなら作らないほうがいい
+        desc = (HIKorokoroDescStruct)(new HIKorokoroDesc());
+    }
+
+    // -- DescStructオブジェクトを取得する
+    public override CsObject GetDescStruct()
+    {
+        return desc;
+    }
+
+    // -- DescオブジェクトをNewして返す
+    public override CsObject CreateDesc()
+    {
+        return new HIKorokoroDesc();
+    }
+
+    // -- DescStructをDescに適用する
+    public override void ApplyDesc(CsObject from, CsObject to)
+    {
+        (from as HIKorokoroDescStruct).ApplyTo(to as HIKorokoroDesc);
+    }
+
+    // -- Sprオブジェクトの構築を行う
+    public override ObjectIf Build()
+    {
+        if (hiSdk == null) { hiSdk = HISdkIf.CreateSdk(); }
+
+        DRCyUsb20Sh4Desc cyDesc = new DRCyUsb20Sh4Desc();
+        for (int i = 0; i < 10; ++i)
+        {
+            cyDesc.channel = i;
+            hiSdk.AddRealDevice(DRCyUsb20Sh4If.GetIfInfoStatic(), cyDesc);
+        }
+
+     
+
+        //
+
+        DRUARTMotorDriverDesc umDesc = new DRUARTMotorDriverDesc();
+       DRUARTMotorDriverIf uartMotorDriver = (DRUARTMotorDriverIf)hiSdk.AddRealDevice(DRUARTMotorDriverIf.GetIfInfoStatic(), umDesc);
+        hiSdk.AddRealDevice(DRKeyMouseWin32If.GetIfInfoStatic());
+
+        //
+
+        var korokoro = hiSdk.CreateHumanInterface(HIKorokoroIf.GetIfInfoStatic());
+
+
+        var d = new HIKorokoroDesc(); // do not use "desc"
+
+        for (int i = 0; i < nMotors; i++)
+        {
+            var dM = new HIKorokoroMotorDesc();
+            d.motors.push_back(dM);
+        }
+        
+            korokoro.Init(d);
+        
+
+        
+
+        return korokoro;
+    }
+
+    
+
+
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // MonoBehaviourのメソッド
+
+    void FixedUpdate()
+    {
+       
+        if (sprObject != null)
+        {
+            //haptic pointerの位置をpointerと合わせる
+            //controllerTransform = GameObject.Find("Sphere").transform;
+            //controllerTransform.position = pointerTransform.position;
+            //controllerTransform.rotation = pointerTransform.rotation;
+
+            //pointerの位置をSetPose
+            pointerTransform = GameObject.Find("Pointer").transform;
+            Posed pointerPose = new Posed(pointerTransform.position.ToVec3d(), pointerTransform.rotation.ToQuaterniond());
+  
+            //if (hiKorokoro.IsGood())
+            {
+                hiKorokoro.SetPose(pointerPose);
+                //Debug.Log("SetPose" + pointerPose);
+            }
+
+
+            if (lengthText != null)
+            {
+                string text = "";
+                for (int i = 0; i < (int)hiKorokoro.NMotor(); i++)
+                {
+                    text += hiKorokoro.GetMotor((uint)i).ToString() + "\r\n";
+                    
+                }
+                lengthText.text = text;
+            }
+
+            //if (EditorApplication.isPlaying == false)
+            //{
+            //    // Play終わったら
+            //    Build().Clear();
+            //    Debug.Log("clear");
+            //}
+
+
+            if (pointer != null)
+            {
+
+                Posed pose = hiKorokoro.GetPose();
+                pointer.transform.position = pose.Pos().ToVector3();
+                pointer.transform.rotation = pose.Ori().ToQuaternion();
+            }
+        }
+    }
+
+    
+}
